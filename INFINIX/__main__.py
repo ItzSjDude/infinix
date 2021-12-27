cdb3b4a94ec2ed66870f6ff78de414b031b56c2a
Sachin Jangir <support@ItzSjDude.in>
2021-12-27 08:52:21 +0530

    Update __main__.py

--- INFINIX/__main__.py
+++ INFINIX/__main__.py
@@ -1,5 +1,5 @@
-from . import infinix
+from . import StartInfinix
 import asyncio as _asyncio  
 
 if __name__ == "__main__": 
-    _asyncio.get_event_loop().run_until_complete(infinix())
+    _asyncio.get_event_loop().run_until_complete(StartInfinix())
