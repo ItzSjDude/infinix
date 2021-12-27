dc3e0c4bbe3ef370ae6ff29e1783ccf1da29a763
Sachin Jangir <support@ItzSjDude.in>
2021-12-27 11:28:22 +0530

    Update login.py

--- INFINIX/startup/login.py
+++ INFINIX/startup/login.py
@@ -10,80 +10,99 @@ from telethon.errors.rpcerrorlist import *
 from logging import getLogger
 infxlog = getLogger("Infinix Login")
 
+from telethon import TelegramClient
+import asyncio
+import os
+import sys
+from ...core import pdb, pset
+from ...clients import *
+from telethon import TelegramClient, events, custom
+from telethon.sessions import StringSession
+from telethon.errors.rpcerrorlist import *
+from logging import getLogger
+pikalog = getLogger("LOGIN_ASSISTENT")
+
 # ----------------------------------Constants--------------------------------
-_phone_ = "**Login Assistent** For {}\n\nEnter your Phone no. On which u want Infinix Userbot 😛\nIf Indian No. **+91xxxxxxxxxx** else use **Country Code**"
+_phone_ = "**Login Assistent** For {}\n\nEnter your Phone no. On which u want @PikachuUserbot 😛\nIf Indian No. **+91xxxxxxxxxx** else use **Country Code**"
 _2vfa_ = "**Login Assistent** For {}\n\nSeems like u have **2-Step verification** On your Account. Enter Your Password"
 _verif_ = "**Login Assistent** For {}\n\nPlease enter the verification code that you receive from Telegram\n**if your code is** 06969 **then enter** 0 6 9 6 9."
 _code_ = "**Login Assistent** For {}\n\n**Invalid Code Received**. Please /start"
-_logged_ = "Login Assistent** For {}\n\n {}:LOGGED IN\n\nwait for 1Min n then Do `.ping`"
+_logged_ = "Login Assistent** For {}\n\n {}:LOGGED IN\n\nwait for 1Min n then Do `.pika`"
 # ----------------------------------Constants--------------------------------
  
-def get_cl_name(_infx_):
-    if _infx_ == "alpha":
+def get_cl_name(_PiKa_):
+    if _PiKa_ == "alpha":
         return "Mainclient"
-    if _infx_ == "beta":
+    if _PiKa_ == "beta":
         return "Multiclient1"
-    if _infx_ == "gaama":
+    if _PiKa_ == "gaama":
         return "Multiclient2"
-    if _infx_ == "delta":
+    if _PiKa_ == "delta":
         return "Multiclient3"
 
-async def infx_login(_infx_):
+async def infx_login(_PiKa_):
     tgclient= tg_client = tgbot_client = tgbot
-    _cn_=get_cl_name(_infx_)
+    _cn_=get_cl_name(_PiKa_)
     async with tgbot_client:
         me = await tgbot_client.get_me()
-        infxlog.info(me.first_name)
+        pikalog.info(me.first_name)
         
         @tgclient.on(events.NewMessage())
         async def handler(tglogin):
             if not tglogin.is_private:
                 return
-            async with tg_client.conversation(tglogin.chat_id) as inflogin:
-                await inflogin.send_message(_phone_.format(_cn_))
-                infxres = await inflogin.get_response()
-                phone = infxres.message.strip()
-                infx_client = TelegramClient(
+            async with tg_client.conversation(tglogin.chat_id) as pikulogin:
+                await pikulogin.send_message(_phone_.format(_cn_))
+                pikaget = pikulogin.wait_event(events.NewMessage(
+                    chats=tglogin.chat_id
+                ))
+                pikares = await pikaget
+                phone = pikares.message.message.strip()
+                pika_client = TelegramClient(
                     StringSession(),
                     api_id=pdb.Api_id,
                     api_hash=pdb.Api_hash
                 )
-                await infx_client.connect()
-                try: 
-                    await infx_client.send_code_request(phone)
-                except Exception as e:
-                    infxlog.info(str(e))
-                await inflogin.send_message(_verif_.format(_cn_))
-                infxlog.info(
+
+                await pika_client.connect()
+                await pika_client.send_code_request(phone)
+                await pikulogin.send_message(_verif_.format(_cn_))
+                pikalog.info(
                     "{}: Please enter the verification code, by giving space. If your code is 6969 then Enter 6 9 6 9".format(_cn_))
-                rap=await inflogin.get_response()
-                r_code = rsp.message.strip()
+                response = pikulogin.wait_event(events.NewMessage(
+                    chats=tglogin.chat_id
+                ))
+                response = await response
+                r_code = response.message.message.strip()
                 _2vfa_code_ = None
                 r_code = "".join(r_code.split(" "))
                 try:
-                    await infx_client.sign_in(phone, code=r_code, password=_2vfa_code_)
-                    s_string = infx_client.session.save()
-                    infx_me = await infx_client.get_me()
-                    await inflogin.send_message(_logged_.format(_cn_, infx_me.first_name))
-                    infxlog.info(
-                        f"Successfully Logged in as {infx_me.first_name}")
-                    pset(f"{_infx_}", "session", s_string)
+                    await pika_client.sign_in(phone, code=r_code, password=_2vfa_code_)
+                    s_string = pika_client.session.save()
+                    pika_me = await pika_client.get_me()
+                    await pikulogin.send_message(_logged_.format(_cn_, pika_me.first_name))
+                    pikalog.info(
+                        f"Successfully Logged in as {pika_me.first_name}")
+                    pset(f"{_PiKa_}", "session", s_string)
                 except PhoneCodeInvalidError:
-                    await inflogin.send_message(_code_.format(_cn_,))
+                    await pikulogin.send_message(_code_.format(_cn_,))
                     return
                 except SessionPasswordNeededError:
-                    infxlog.info(
+                    pikalog.info(
                         "{}: 2-Step verification Protected Account, Enter Your Password".format(_cn_))
-                    await inflogin.send_message(_2vfa_.format(_cn_))
-                    rspp = await inflogin.get_response()
-                    _2vfa_code_ = rrpp.message.strip()
-                    await infx_client.sign_in(password=_2vfa_code_)
-                    infx_me = await infx_client.get_me()
-                    infxlog.info(
-                        f"Successfully Logged in as {infx_me.first_name}")
-                    s_string = infx_client.session.save()
-                    await inflogin.send_message(_logged_.format(_cn_, infx_me.first_name))
-                    pset(f"{_infx_}", "session", s_string)
+                    await pikulogin.send_message(_2vfa_.format(_cn_))
+                    response = pikulogin.wait_event(events.NewMessage(
+                        chats=tglogin.chat_id
+                    ))
+                    response = await response
+                    _2vfa_code_ = response.message.message.strip()
+                    await pika_client.sign_in(password=_2vfa_code_)
+                    pika_me = await pika_client.get_me()
+                    pikalog.info(
+                        f"Successfully Logged in as {pika_me.first_name}")
+                    s_string = pika_client.session.save()
+                    await pikulogin.send_message(_logged_.format(_cn_, pika_me.first_name))
+                    pset(f"{_PiKa_}", "session", s_string)
                     
         await tgclient.run_until_disconnected()
 
