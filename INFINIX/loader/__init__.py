3898d8dcf9cab28903b65fa44014662368b71770
Sachin Jangir <support@ItzSjDude.in>
2022-03-02 18:03:13 +0530

    Update __init__.py

--- INFINIX/loader/__init__.py
+++ INFINIX/loader/__init__.py
@@ -13,36 +13,42 @@ from pathlib import Path as _asstpath
 from logging import getLogger
 logpl = getLogger("Plugins:")
 logpa = getLogger("Assistant:")
+bfiles=glob.glob("./plugins/*.py")
+afiles=glob.glob("./assistant/plugins/*.py")
 
-def infx_assistant(_infxsst=None):
-    rx = "!"
-    import plugins.__init__ as _Modules
-    asstpath = _asstpath(f"./infinix/Assistant/plugins/{_infxsst}.py")
-    asstname = "infinix.Assistant.plugins.{}".format(_infxsst)
-    spec = spec_from_file_location(asstname, asstpath)
-    asst = module_from_spec(spec)
-                               #____infinix__Assistant__Plugins__Loader____
-    userbot = infinix; asst.bot = bot; asst.tgbot = tgbot; asst.rx = rx; asst.Infinix = Infinix; asst.pdb = pdb; asst.infxtgbot = infxtgbot; modules['Asst_modules'] = _Modules       
-    infxast[_infxsst] = asst; modules["infinix"+_infxsst] = asst; tgbot.infxast[_infxsst] = asst; spec.loader.exec_module(asst); logpa.info("🔥Imported "+_infxsst)
-           
-
-def infx_plugins(_infxmod=None):
+def load_infx(_infxmod=None):
     from pathlib import Path
     from ..database import pdb,pget,pset
+    import plugins.__init__ as _Modules
+
+    rx = "!"; alf=False 
+    if _infxmod=="ubplugs":
+       files=bfiles
+       alf=True 
+    if _infxmod=="astplugs":
+       files=afiles
+
+    for name in files:
     for client in infclts:
-        
-      _rx=pget("alpha", "cmdhandler")
-      if _rx: 
-          rx = _rx
-      else: 
-          rx = "."
-      path = Path(f"plugins/{_infxmod}.py")
-      name = "plugins.{}".format(_infxmod)
-      spec = spec_from_file_location(name, path)
-      _infx = module_from_spec(spec)
-                                   #____infinix__Plugins__Loader____
-      userbot = INFINIX; _infx.bot = client; _infx.rx = rx; _infx.Infinix = Infinix; _infx.pdb = pdb; _infx.borg = client; _infx.logger = logging.getLogger(_infxmod)
-      modules["userbot"] = INFINIX; modules["userbot.utils"] = _utilz; spec.loader.exec_module(_infx); client.infx_cmd[_infxmod] = _infx; modules["infinix"+_infxmod] = _infx; logpl.info("🔥Imported "+_infxmod)
+        with open(name) as f:
+            path1 = Path(f.name); shortname = path1.stem; flik=glk=None
+            blik=shortname.replace(".py", "")
+            if alt:
+                flik=f"plugins/{blik}.py"
+                glk=True
+            else: 
+                flik=f"assistant/plugins/{blik}.py"
+                 
+            spec = spec_from_file_location("{}".format(flik.replace(".py", ""), Path(flik))
+            _infx = module_from_spec(spec)
+            asst = module_from_spec(spec)
+            if glk: 
+                userbot = INFINIX; _infx.bot = client; _infx.rx = rx; _infx.Infinix = Infinix; _infx.pdb = pdb; _infx.borg = client; _infx.logger = logging.getLogger(blik)
+                modules["userbot"] = INFINIX; modules["userbot.utils"] = _utilz; spec.loader.exec_module(_infx); client.infx_cmd[blik] = _infx; modules["infinix"+_infx] = _infx; logpl.info("🔥Imported "+_infxmod)
+            else: 
+                userbot = INFINIX; asst.bot = client; asst.tgbot = tgbot; asst.rx = rx; asst.Infinix = Infinix; asst.pdb = pdb; asst.infxtgbot = infxtgbot; modules['Asst_modules'] = _Modules       
+                infxast[_infxsst] = asst; modules["infinix"+_infxsst] = asst; tgbot.infxast[_infxsst] = asst; spec.loader.exec_module(asst); logpa.info("🔥Imported "+_infxsst)
+
 
 def load_ext_module(shortname):
     if shortname.endswith("_"):
