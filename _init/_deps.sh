70422e05f9b04fe9197013d2e2742fb9b05a530b
Sachin Jangir <support@ItzSjDude.in>
2022-08-11 19:42:49 +0530

    Update _deps.sh

--- _init/_deps.sh
+++ _init/_deps.sh
@@ -17,8 +17,7 @@ _UpSource() {
     echo 'Github: Updating Your INFINIX With ItzSjDude/Infinix' 
     git init .     
     git remote add origin https://github.com/ItzSjDude/infinix.git  
-    git fetch     
-    git checkout -t origin/main -f
+    git pull origin main --allow-unrelated-histories
     git clone -b Beta https://github.com/ItzSjDude/PikaBotPlugins ./Temp
     mkdir ./plugins
     cp ./Temp/plugins/*.py ./plugins
