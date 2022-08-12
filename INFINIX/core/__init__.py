8bb814df7f1f9830c021871d6b35871fb5f2337d
Sachin Jangir <support@ItzSjDude.in>
2022-08-12 08:54:45 +0530

    Update __init__.py

--- INFINIX/core/__init__.py
+++ INFINIX/core/__init__.py
@@ -7,23 +7,16 @@ Id5="#cHRnYm90"
 #_____________________
 
 from telethon import TelegramClient
+if _corelib: from pyrogram import Client 
 from telethon.sessions import StringSession
 from ..database import pdb
 def InfxClient(session, gBot=None):
-    if gBot:
-        return TelegramClient(
-            "TgBot",
-            pdb.Api_id,
-            pdb.Api_hash).start(
-            bot_token=session)
+    if _corelib: 
+       if gBot: return Client("PtgBot",api_id=pdb.Api_id,api_hash=pdb.Api_hash,bot_token=session)
+       else: return Client(api_id=pdb.Api_id,api_hash=pdb.Api_hash,session_string=session) 
     else:
-        return TelegramClient(
-            StringSession(session),
-            pdb.Api_id,
-            pdb.Api_hash,
-            connection_retries=None,
-            auto_reconnect=True,
-            lang_code='en')
+       if gBot: return TelegramClient("TtgBot",pdb.Api_id,pdb.Api_hash).start(bot_token=session)
+       else: return TelegramClient(StringSession(session),pdb.Api_id,pdb.Api_hash,connection_retries=None,auto_reconnect=True,lang_code='en')
 
 def gpcid(_id_):
     if _id_==Id1:
