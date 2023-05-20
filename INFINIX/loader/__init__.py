336f01db96ea6bc5b23e34823f4599e4f2c35183
Sachin Jangir <support@ItzSjDude.in>
2023-05-21 03:22:50 +0530

    Update __init__.py

--- INFINIX/loader/__init__.py
+++ INFINIX/loader/__init__.py
@@ -28,26 +28,27 @@ def load_infx(_infxmod=None):
        files=afiles
 
     for name in files:
-        for client in infclts:
-            with open(name) as f:
-                path1 = Path(f.name); shortname = path1.stem; flik=glk=nx=None
-                blik=shortname.replace(".py", "")
-                if alf:
-                    flik=f"plugins/{blik}.py"
-                    nx=f"plugins.{blik}"
-                    glk=True
-                else: 
-                    flik=f"assistant/plugins/{blik}.py"
-                    nx=f"assistant.plugins.{blik}"
-                spec = spec_from_file_location(nx,Path(flik))
-                _infx = module_from_spec(spec)
-                asst = module_from_spec(spec)
-                if glk: 
-                    userbot = INFINIX; _infx.bot = client; _infx.rx = rx; _infx.Infinix = Infinix; _infx.pdb = pdb; _infx.borg = client; _infx.logger = logging.getLogger(blik)
-                    modules["userbot"] = INFINIX; modules["userbot.utils"] = _utilz; spec.loader.exec_module(_infx); client.infx_cmd[blik] = _infx; modules["infinix"+blik] = _infx; logpl.info(f"🔥Imported {blik}")
-                else: 
-                    userbot = INFINIX; asst.bot = client; asst.tgbot = tgbot; asst.rx = rx; asst.Infinix = Infinix; asst.pdb = pdb; asst.infxtgbot = infxtgbot; modules['Asst_modules'] = _Modules       
-                    infxast[_infxsst] = asst; modules["infinix"+_infxsst] = asst; tgbot.infxast[_infxsst] = asst; spec.loader.exec_module(asst); logpa.info("🔥Imported "+_infxsst)
+       
+        with open(name) as f:
+            path1 = Path(f.name); shortname = path1.stem; flik=glk=nx=None
+            blik=shortname.replace(".py", "")
+            if alf:
+                flik=f"plugins/{blik}.py"
+                nx=f"plugins.{blik}"
+                glk=True
+            else: 
+                flik=f"assistant/plugins/{blik}.py"
+                nx=f"assistant.plugins.{blik}"
+            spec = spec_from_file_location(nx,Path(flik))
+            _infx = module_from_spec(spec)
+            asst = module_from_spec(spec)
+           
+            if glk: 
+                userbot = INFINIX; _infx.bot = bot; _infx.rx = rx; _infx.Infinix = Infinix; _infx.pdb = pdb; _infx.borg = client; _infx.logger = logging.getLogger(blik)
+                modules["userbot"] = INFINIX; modules["userbot.utils"] = _utilz; spec.loader.exec_module(_infx); client.infx_cmd[blik] = _infx; modules["infinix"+blik] = _infx; logpl.info(f"🔥Imported {blik}")
+            else: 
+                userbot = INFINIX; asst.bot = client; asst.tgbot = tgbot; asst.rx = rx; asst.Infinix = Infinix; asst.pdb = pdb; asst.infxtgbot = infxtgbot; modules['Asst_modules'] = _Modules       
+                infxast[_infxsst] = asst; modules["infinix"+_infxsst] = asst; tgbot.infxast[_infxsst] = asst; spec.loader.exec_module(asst); logpa.info("🔥Imported "+_infxsst)
 
 
 def load_ext_module(shortname):
