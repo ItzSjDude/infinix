b05fda239ee06028842ec531da22f85e22d2976d
Sachin Jangir <support@ItzSjDude.in>
2023-05-21 03:33:31 +0530

    Update __init__.py

--- INFINIX/loader/__init__.py
+++ INFINIX/loader/__init__.py
@@ -44,9 +44,11 @@ def load_infx(_infxmod=None):
             asst = module_from_spec(spec)
            
             if glk: 
+                client = bot
                 userbot = INFINIX; _infx.bot = bot; _infx.rx = rx; _infx.Infinix = Infinix; _infx.pdb = pdb; _infx.borg = client; _infx.logger = logging.getLogger(blik)
                 modules["userbot"] = INFINIX; modules["userbot.utils"] = _utilz; spec.loader.exec_module(_infx); client.infx_cmd[blik] = _infx; modules["infinix"+blik] = _infx; logpl.info(f"🔥Imported {blik}")
             else: 
+                client = tgbot
                 userbot = INFINIX; asst.bot = tgbot; asst.tgbot = tgbot; asst.rx = rx; asst.Infinix = Infinix; asst.pdb = pdb; asst.infxtgbot = infxtgbot; modules['Asst_modules'] = _Modules       
                 infxast[_infxsst] = asst; modules["infinix"+_infxsst] = asst; tgbot.infxast[_infxsst] = asst; spec.loader.exec_module(asst); logpa.info("🔥Imported "+_infxsst)
 
