3302ed71bd4fa05a67b9f896f58df827117d4996
Sachin Jangir <support@ItzSjDude.in>
2023-05-21 04:08:34 +0530

    Update __init__.py

--- INFINIX/loader/__init__.py
+++ INFINIX/loader/__init__.py
@@ -52,13 +52,9 @@ def load_infx(_infxmod=None):
                 userbot = INFINIX; asst.bot = tgbot; asst.tgbot = tgbot; asst.rx = rx; asst.Infinix = Infinix; asst.pdb = pdb; asst.infxtgbot = infxtgbot; modules['Asst_modules'] = _Modules       
                 infxast[_infxsst] = asst; modules["infinix"+_infxsst] = asst; tgbot.infxast[_infxsst] = asst; spec.loader.exec_module(asst); logpa.info("🔥Imported "+_infxsst)
             
-            if bot2: 
-                client = bot2
-                userbot = INFINIX; _infx.bot = bot2; _infx.rx = rx; _infx.Infinix = Infinix; _infx.pdb = pdb; _infx.borg = client; _infx.logger = logging.getLogger(blik)
-                modules["userbot"] = INFINIX; modules["userbot.utils"] = _utilz; spec.loader.exec_module(_infx); client.infx_cmd[blik] = _infx; modules["infinix"+blik] = _infx; logpl.info(f"🔥Imported {blik}")
             
-
-def load_ext_module(shortname):
+           
+ def load_ext_module(shortname):
     if shortname.endswith("_"):
         from pathlib import Path
         path = Path(f"plugins/{shortname}.py")
