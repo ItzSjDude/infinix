ac1fbf0cac55dcba606c9438d15b8a39254fd20a
Sachin Jangir <support@ItzSjDude.in>
2023-05-30 16:15:52 +0530

    Update __main__.py

--- INFINIX/__main__.py
+++ INFINIX/__main__.py
@@ -4,14 +4,5 @@ import sys
 import asyncio as o
 
 if __name__ == "__main__": 
-    if sys.version_info < (3, 10):
-        a = o.get_event_loop()
-    else:
-        try:
-            a = o.get_running_loop()
-        except RuntimeError:
-            a = o.new_event_loop()
-
-    o.set_event_loop(a)
-    a.run_until_complete(StartInfinix())
+       o.run(StartInfinix())
    
