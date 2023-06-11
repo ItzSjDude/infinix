2d2004a3f1ad9acd55ce15e38690511519a28a33
Sachin Jangir <support@ItzSjDude.in>
2023-06-12 00:54:21 +0530

    Update wrapper.py

--- INFINIX/utils/infxclient/wrapper.py
+++ INFINIX/utils/infxclient/wrapper.py
@@ -35,7 +35,7 @@ async def infxgvar(_infx_, value=None):
   else: 
       return a
 async def infx_msg(_infx, text, _infx_=None, parse_mode=None, link_preview=None):
-  parse_mode = parse_mode or "md"; link_preview = link_preview or False
+  parse_mode = parse_mode; link_preview = link_preview or False
   try: 
      _reply = await _infx.get_reply_message()
   except: 
